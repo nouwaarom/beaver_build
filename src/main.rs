da87f23ba02248ecb5289c73be653db07a7ec1f6
Elbert van de Put <elbert@t-matix.nl>
2023-09-26 23:45:38 +0200

    fundamentals: PoC to start a process

---
+++ src/main.rs
@@ -0,0 +1,31 @@
+use std::process::Command;
+use nix::unistd::{execv, fork, ForkResult, getpid, write};
+use nix::sys::wait::waitpid;
+use libc::{_exit, STDOUT_FILENO};
+
+fn main() {
+    println!("Hello, world!");
+    let pid = getpid();
+    println!("I am the parent and have pid: {}", pid);
+
+    let output = Command::new("/usr/bin/gcc").arg("--version").output().expect("Failed to execute command");
+
+    let output_string = String::from_utf8(output.stdout.as_slice().to_vec()).expect("Invalid characters in output");
+    println!("GCC stdout: {}", output_string);
+
+    match unsafe{fork()} {
+        Ok(ForkResult::Parent { child, .. }) => {
+            println!("Continuing execution in parent process, new child has pid: {}", child);
+            waitpid(child, None).unwrap();
+        }
+        Ok(ForkResult::Child) => {
+            // Unsafe to use `println!` (or `unwrap`) here. See Safety.
+            let pid = getpid();
+            let message = format!("I'm a new child process and have pid: {}\n", pid);
+            write(libc::STDOUT_FILENO, message.as_bytes()).ok();
+
+            unsafe { libc::_exit(0) };
+        }
+        Err(_) => println!("Fork failed"),
+    }
+}
