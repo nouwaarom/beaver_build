0ce1c84ec39453c2a48de1fb2160205c94cf5af0
Elbert van de Put <elbert@t-matix.nl>
2023-11-14 22:41:39 +0100

    misc: Remove debug statements and dead code

--- src/main.rs
+++ src/main.rs
@@ -12,11 +12,10 @@ use std::time::{Duration, Instant};
 use builder::{Builder}; 
 use configurator::{configure_clib_project};
 use graph_walker::{GraphWalker, GraphVisitor};
-use work_pool::thread_pool_test;
 use work_pool::{WorkPool, WorkInstruction};
 
 fn main() {
-    println!("Hello, world!");
+    println!("Beavers will start building!");
 
     let mut build_directory = env::current_dir().unwrap();
     build_directory.push("beaver_build_debug");
@@ -66,26 +65,4 @@ fn main() {
     let duration = start.elapsed();
 
     println!("Build time is: {} s", duration.as_secs_f32());
-
-    /*
-    execute_task();
-    //let pid = getpid();
-    //println!("I am the parent and have pid: {}", pid);
-
-    match unsafe{fork()} {
-        Ok(ForkResult::Parent { child, .. }) => {
-            println!("Continuing execution in parent process, new child has pid: {}", child);
-            waitpid(child, None).unwrap();
-        }
-        Ok(ForkResult::Child) => {
-            // Unsafe to use `println!` (or `unwrap`) here. See Safety.
-            let pid = getpid();
-            let message = format!("I'm a new child process and have pid: {}\n", pid);
-            write(libc::STDOUT_FILENO, message.as_bytes()).ok();
-
-            unsafe { libc::_exit(0) };
-        }
-        Err(_) => println!("Fork failed"),
-    }
-    */
 }
